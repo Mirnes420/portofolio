0f5235a23637059bcc6c9764996c69ab47f936b9
Mirnes Nuhanović <mirnesnuhanovic9@gmail.com>
2025-09-07 01:57:12 +0200

    removed the chatbot project entirely

--- djangoPort/urls.py
+++ djangoPort/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import *
-from . import chatbot
 
 urlpatterns = [
     path("", portfolio, name="portfolio"),
-    path("chatbot/", chatbot.chatbot, name="chatbot"), 
     path("contact/", contact, name="contact")
 ]
